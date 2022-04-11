fa10309dd14736497481c0666c846fa494a61175
old-denki-github-pages <87896075+DenkiOfficial-Coding@users.noreply.github.com>
2022-04-11 21:23:20 +0700

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -12,14 +12,24 @@ module.exports = {
 
     let helpembed = new MessageEmbed()
     helpembed.setColor("RANDOM")
-      helpembed.setAuthor({ name: `Commands of ${client.user.username}`, iconURL: client.user.avatarURL() })
+      helpembed.setAuthor(
+          `Commands of ${client.user.username}`
+        )
       helpembed.setColor("#2f3136")
       helpembed.setThumbnail("https://cdn.discordapp.com/attachments/956850904311664673/960016245208514560/ayame.gif")
       helpembed.addField(
         `<a:cmc_bonk:949563152398426132> <a:cmc_muiten:949219236004388884> **COMMANDS LIST** <a:SU_muiten2:960076330865680414> <a:cmc_bonk:949563152398426132>`,
-        `${client.commands.map(c => `${c.name}`).join("\n")}`,
+        `🎉 Giveaway
+        - drop, end, start, reroll
+        📷 SFW
+        - baka, cat, cuddle, dog, feed, foxgirl, gecg, goose, wallpaper, slap, tickle
+        - waifu, woof, lizard, meow, neko, nekogif, pat, poke, holo, hug, kiss, smug
+        🤷‍♂️ Infomation
+        - avatar, bug-report, stats, ping, server, short, help, invite
+        🔞 NSFW
+        - boobs, classic, cumsluts, trap, hentai, kemonomimi, kuni, lesbian, pussy, random-anime`,
       );
-      helpembed.setFooter({text: `Ayame Version 2`});
+      helpembed.setFooter(`Ayame Version 2`);
 
       const row = new MessageActionRow().addComponents(
 				new MessageButton()
